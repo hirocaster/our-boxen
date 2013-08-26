656ab37cd954755764d662911896787c71a62718
hirocaster <hohtsuka@gmail.com>
2013-08-26 14:15:14 +0900

    Add sudo plugin for rbenv

--- modules/people/manifests/hirocaster.pp
+++ modules/people/manifests/hirocaster.pp
@@ -1,4 +1,8 @@
 class people::hirocaster {
+  ruby::plugin { 'rbenv-sudo':
+    ensure => 'master',
+    source  => 'dcarley/rbenv-sudo'
+  }
   class { 'nodejs::global': version => 'v0.10' }
   nodejs::module { 'bower': node_version => 'v0.10' }
   nodejs::module { 'ninja': node_version => 'v0.10' }
