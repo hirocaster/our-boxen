15f8a95b73eb0ad69e0b042665d81323ac659f6e
hirocaster <hohtsuka@gmail.com>
2015-08-23 15:52:02 +0900

    Merge from origin/master

--- modules/people/manifests/hirocaster.pp
+++ modules/people/manifests/hirocaster.pp
@@ -1,28 +1,61 @@
 class people::hirocaster {
-  # ruby::plugin { 'rbenv-sudo':
-  #   ensure => 'master',
-  #   source => 'dcarley/rbenv-sudo'
-  # }
-  class { 'nodejs::global': version => 'v0.10.1' }
-  nodejs::module { 'yo': node_version => 'v0.10' }
-  nodejs::module { 'grunt-cli': node_version => 'v0.10' }
-  nodejs::module { 'bower': node_version => 'v0.10' }
-  nodejs::module { 'generator-angular': node_version => 'v0.10' }
-  nodejs::module { 'ninja': node_version => 'v0.10' }
-  nodejs::module { 'ungit': node_version => 'v0.10' }
+  # Node.js
+  # FIX: 昔入れたnvmモジュールの残骸を削除
+  file { '/opt/boxen/bin/node':
+    ensure => absent
+  }
+  file { '/opt/boxen/bin/npm':
+    ensure => absent
+  }
 
+  include nodejs
+  $version = "0.12" # specific to hirocaster.yaml
+  class { 'nodejs::global':
+    version => $version
+  }
+  # TODO: faild install
+  # npm_module { 'bower for ${version}':
+  #   module       => 'bower',
+  #   node_version => $version,
+  # }
+  # npm_module { 'yo for ${version}':
+  #   module       => 'yo',
+  #   node_version => $version,
+  # }
+  # npm_module { 'grunt-cli for ${version}':
+  #   module       => 'grunt-cli',
+  #   node_version => $version,
+  # }
+  # npm_module { 'generator-angular for ${version}':
+  #   module       => 'generator-angular',
+  #   node_version => $version,
+  # }
+  # npm_module { 'ninja for ${version}':
+  #   module       => 'ninja',
+  #   node_version => $version,
+  # }
+
+  # Ruby
+  include ruby
   class { 'ruby::global':
     version => 'system'
   }
-
+  ruby::version { '2.2.2': }
   ruby_gem { "bundler for all ruby version":
     gem          => 'bundler',
     version      => '~> 1.0',
     ruby_version => '*',
   }
-
   ruby_gem { "ghn for all ruby version":
     gem          => 'ghn',
+    ruby_version => '*',
+  }
+  ruby_gem { "rubocop for all ruby version":
+    gem          => 'rubocop',
+    ruby_version => '*',
+  }
+  ruby_gem { "ruby-lint for all ruby version":
+    gem          => 'ruby-lint',
     ruby_version => '*',
   }
 
@@ -31,16 +64,19 @@ class people::hirocaster {
   # python
   include python
   package { 'pyenv-virtualenv': }
-  python::pip { 'percol':
+  python::pip { 'mycli':
     virtualenv => '/opt/boxen/homebrew/',
   }
 
+  # homebrew
+  include brewcask
   homebrew::tap { 'railwaycat/emacsmacport': }
-  homebrew::tap { 'homebrew/dupes': }
-  package { 'homebrew/dupes/apple-gcc42': }
+  # homebrew::tap { 'homebrew/dupes': }
+  # package { 'homebrew/dupes/apple-gcc42': }
 
-  include atom
-  atom::package { 'markdown-preview': }
-  atom::package { 'markdown-scroll-sync': }
-  atom::package { 'japanese-wrap': }
+  # include atom
+  # atom::package { 'markdown-preview': }
+  # atom::package { 'markdown-scroll-sync': }
+  # atom::package { 'japanese-wrap': }
+  # atom::package { 'autosave': }
 }
